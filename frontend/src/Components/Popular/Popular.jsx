5d18f3a0c2c66707505ae516b53940ffddafd97b
Ancyl Lionel Dsouza <116951285+aancyl@users.noreply.github.com>
2025-06-04 11:53:55 +0400

    Update Popular.jsx

--- frontend/src/Components/Popular/Popular.jsx
+++ frontend/src/Components/Popular/Popular.jsx
@@ -7,7 +7,7 @@ const Popular = () => {
 
     const [popularProducts, setPopularProducts] = useState([]);
     useEffect(()=>{
-        fetch('http://localhost:4000/popular')
+        fetch('https://ecospin-ecommerce-backend.onrender.com/popular')
         .then((response)=>response.json())
         .then((data)=>setPopularProducts(data))
     },[]);
