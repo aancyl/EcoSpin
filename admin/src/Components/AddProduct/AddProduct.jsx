f791ccc7ce449970e2ff022eafe445179310e8e8
Ancyl Lionel Dsouza <116951285+aancyl@users.noreply.github.com>
2025-06-04 11:49:11 +0400

    Update AddProduct.jsx

--- admin/src/Components/AddProduct/AddProduct.jsx
+++ admin/src/Components/AddProduct/AddProduct.jsx
@@ -31,7 +31,7 @@ const AddProduct = () => {
         let formData = new FormData();
         formData.append('product', image);
 
-        await fetch('http://localhost:4000/upload', {
+        await fetch('https://ecospin-ecommerce-backend.onrender.com/upload', {
             method: 'POST',
             headers: {
                 Accept: 'application/json' 
@@ -41,7 +41,7 @@ const AddProduct = () => {
 
         if (responseData.success) {
             product.image = responseData.image_url;
-            await fetch('http://localhost:4000/addproduct', {
+            await fetch('https://ecospin-ecommerce-backend.onrender.com/addproduct', {
                 method: 'POST',
                 headers: {
                     Accept: 'application/json',
